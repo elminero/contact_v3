c3ad78a8307a0ff05fdb6185158bcbc31b668ba3
Robert Farber <elminero@cox.net>
2015-04-19 21:03:17 +0000

    Modified format of views/person/list.php

--- views/person/list.php
+++ views/person/list.php
@@ -1,16 +1,55 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Ian
- * Date: 4/17/2015
- * Time: 6:41 PM
- */
+use yii\helpers\Html;
 
 
 
 
 foreach($namesWithAddress as $name) {
-   echo $name["last_name"] . $name["first_name"] . $name["middle_name"] . $name["alias_name"] . $name["street"] . $name["city"] . $name["state"] . $name["postal_code"] . $name["iso"] . "<br />";
+
+    $nameAddress =
+
+    $name["last_name"];
+
+    if($name["last_name"] && $name["first_name"]) $nameAddress .= ", " . $name["first_name"];
+
+    if( ($name["last_name"] || $name["first_name"]) && $name["middle_name"]) $nameAddress .= ", " . $name["middle_name"];
+
+    if($name["alias_name"])  $nameAddress .= "<br />Alias: " . $name["alias_name"];
+
+
+    $nameAddress .= "<br />";
+
+    $nameAddress .= $name["street"];
+
+    if($name["street"] && $name["city"]) $nameAddress .= ", " . $name["city"];
+
+    if( ($name["street"] || $name["city"]) && $name["state"]) $nameAddress .= ", " . $name["state"]. " " . $name["postal_code"] . " " .$name["iso"];
+
+
+  //  echo $nameAddress; ['post/index', 'page' => 2]
+
+
+    echo Html::a($nameAddress, ['person/profile', 'id'=>$name["id"]], ['class' => '']);
+
+
+    echo "<hr />";
+
+/*
+        . $name["first_name"] . $name["middle_name"] . $name["alias_name"] .
+
+
+
+
+
+
+
+
+
+
+        $name["street"] . $name["city"] . $name["state"] . $name["postal_code"] . $name["iso"] . "<br />";
+*/
+
+
 
 }
 
